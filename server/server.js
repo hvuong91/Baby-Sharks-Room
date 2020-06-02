9d4e3db3c38a0908aa009bd386a905ea62041de9
HV <hoavuong1991@gmail.com>
2020-06-02 10:31:02 -0700

    Removed unused code

--- server/server.js
+++ server/server.js
@@ -42,18 +42,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// set up database
-const pool = new Pool({
-  host: '127.0.0.1',
-  user: 'postgres',
-  password: 'root',
-  port: '5432',
-  database: 'baby_sharks_room_db',
-});
-
-pool.connect();
-
-// callback - checkout a client
-
-
 module.exports = app;
