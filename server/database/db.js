80a435f254157e368c37e39147f9ef22cfe59eab
HV <hoavuong1991@gmail.com>
2020-06-02 10:08:37 -0700

    Added environment variable for database connection

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  host: '127.0.0.1',
-  user: 'postgres',
-  password: 'root',
-  port: '5432',
-  database: 'baby_sharks_room_db',
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT,
+  database: process.env.RDS_DB_NAME,
 });
 
 pool.connect();
